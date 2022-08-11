3d85f683ab17f944676ccf4ae2de350e98746ad9
Arya Vatan-Abadi <arya.vatanabadi@gmail.com>
2022-08-11 09:44:29 -0400

    Created scriptable objects for runes

---
+++ Assets/Scripts/ScriptableObjects/Rune.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Rune", menuName = "ScriptableObjects/Rune", order = 1)]
+public class Rune : ScriptableObject
+{
+   public int Id;
+   public Sprite Sprite;
+}
