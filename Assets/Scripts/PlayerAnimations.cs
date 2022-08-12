979985eae1323c0c25a80402e080b2f3364ed3ee
Raj SS <raj.ss166@gmail.com>
2022-08-12 00:02:36 -0400

    Did so much stuff idk

---
+++ Assets/Scripts/PlayerAnimations.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimations : MonoBehaviour
+{
+    private PlayerMovement playerMovementScript;
+    private SpriteRenderer playerSprite;
+    private float inputHorizontal;
+    private float inputVertical;
+
+    public Sprite idleSprite;
+    public Sprite sideSprite;
+    public Sprite backSprite;
+
+    public ParticleSystem afterImageParticleSystem;
+    public Material[] particleTexture;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMovementScript = GetComponent<PlayerMovement>();
+        playerSprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        inputHorizontal = Input.GetAxis("Horizontal");
+        inputVertical = Input.GetAxis("Vertical");
+
+        if (inputHorizontal > 0)
+        {
+            playerSprite.sprite = sideSprite;
+            afterImageParticleSystem.GetComponent<ParticleSystemRenderer>().material = particleTexture[3];
+            playerSprite.flipX = false;
+        }
+
+        if (inputHorizontal < 0)
+        {
+            playerSprite.sprite = sideSprite;
+            afterImageParticleSystem.GetComponent<ParticleSystemRenderer>().material = particleTexture[2];
+            playerSprite.flipX = true;
+        }
+
+        if (inputHorizontal == 0 && inputHorizontal == 0)
+        {
+            playerSprite.sprite = idleSprite;
+            afterImageParticleSystem.GetComponent<ParticleSystemRenderer>().material = particleTexture[1];
+            playerSprite.flipX = false;
+        }
+
+        if (inputVertical > 0)
+        {
+            afterImageParticleSystem.GetComponent<ParticleSystemRenderer>().material = particleTexture[0];
+            playerSprite.sprite = backSprite;
+        }
+    }
+}
